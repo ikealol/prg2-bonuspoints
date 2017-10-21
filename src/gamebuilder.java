eddbf53e59600af078ed728e9d5779288c5a5060
ikealol <richard.kuenzi@gmail.com>
2017-10-21 09:49:18 +0200

    add project

---
+++ src/gamebuilder.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Richard on 21/10/2017.
+ */
+public class gamebuilder {
+
+    // build gamedashboard
+    static int[][] gamefield = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+
+   static void printgame(){
+        System.out.print(gamefield[2][1]);
+    }
+
+}
---
+++ src/main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Richard on 21/10/2017.
+ */
+public class main {
+}
